ef21c5336acf55ba4bcdc4fe0671b17c57c86594
1917dc <142899763+1917dc@users.noreply.github.com>
2023-09-06 12:32:03 -0300

    ex2 feito

---
+++ C/LISTA4/ex2.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <ctype.h>
+#include <string.h>
+
+int verificacao();
+
+int main(int agrc, char *argv[]){
+    verificacao();
+}
+
+int verificacao(){
+    int tam;
+    char senha[20];
+
+    printf("Digite uma senha: ");
+    scanf("%s", senha);
+    int mai = 0, min = 0, num = 0; 
+
+    if(strlen(senha) < 8){
+        printf("A sua senha deve ter pelo menos 8 caracteres.\n");
+    }
+    for(int i = 0; i < strlen(senha); i++){
+        if(isupper(senha[i]) > 1){
+            mai++;
+        }
+        if(islower(senha[i]) > 1){
+            min++;
+        }
+        if(isdigit(senha[i]) > 1){
+            num++;
+        }
+    }
+    if(mai > 0, min > 0, num > 0){
+        printf("A sua senha é válida!");
+    } else {
+        printf("A sua senha é inválida!");
+    }
+}
