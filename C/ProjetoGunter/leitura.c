c79e7e29606dcceee749ce27959394f950bd0729
1917dc <142899763+1917dc@users.noreply.github.com>
2023-10-05 19:11:18 -0300

    leitura

--- C/ProjetoGunter/leitura.c
+++ C/ProjetoGunter/leitura.c
@@ -2,21 +2,6 @@
 #include <stdlib.h>
 #include <string.h>
 
-/*
-    fazer um programa que lê arquivos em .csv e cria outro arquivo em .txt chamado relatório
-    * esse programa de relatório vai ler os dados e separar cada tipo de dado, e "rankeando".
-*/
-
-typedef struct{
-    int idVendedor;
-    int idVenda;
-    int idEquipe;
-    float valVenda;
-    char *cargo;
-    char *nome;
-
-} pessoa;
-
 int main(int argc, char *argv[]){
     char *buffer;
     int t = 0;
@@ -25,22 +10,22 @@ int main(int argc, char *argv[]){
     FILE *arquivo = fopen("vendas.csv", "r");
 
     if(arquivo == NULL){
-    printf("Erro ao abrir arquivo.\n");
-    return 1;
+        printf("Erro ao abrir arquivo.\n");
+        return 1;
     }
 
     while(!feof(arquivo)){
         c = fgetc(arquivo);
         t++;
     }
+    
+    fseek(arquivo, 0, SEEK_SET);
 
     buffer = malloc(t * sizeof(char));
 
-    fread(&buffer, sizeof(char), t, arquivo);
+    fread(buffer, sizeof(char), t, arquivo);
     printf("%s", buffer);
-    printf("\n");
-
     
-
+    return 0;
 }
 
